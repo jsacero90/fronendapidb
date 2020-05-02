4956c610e6af9663456700e2d9884a1860783e3f
Jhonatan Acero <stiddark@gmail.com>
2020-05-01 22:10:51 -0500

    menu hidden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
+import { TecnicasComponent } from './components/module/tecnicas/tecnicas.component';
+import { RazasComponent } from './components/module/razas/razas.component';
+import { PlanetasComponent } from './components/module/planetas/planetas.component';
+import { PersonajesComponent } from './components/module/personajes/personajes.component';
+import { ObjetosComponent } from './components/module/objetos/objetos.component';
+import { EstadosComponent } from './components/module/estados/estados.component';
+import { EpisodiosComponent } from './components/module/episodios/episodios.component';
+import { InicioComponent } from './components/module/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'episodios', component: EpisodiosComponent},
+  { path: 'estados', component: EstadosComponent},
+  { path: 'objetos', component: ObjetosComponent},
+  { path: 'personajes', component: PersonajesComponent},
+  { path: 'planetas', component: PlanetasComponent},
+  { path: 'razas', component: RazasComponent},
+  { path: 'tecnicas', component: TecnicasComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
